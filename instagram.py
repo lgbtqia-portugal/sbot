lgbtqia-portugal/sbot

commit db98ba9bf4828da480df67d058274110a6f6ab63

instagram: don't save last_timestamp unless it's newer
the instagram API sometimes returns incomplete media,
causing the bot to re-post

instagram.py
@@ -27,8 +27,11 @@ def new_media(bot):
 					continue
 				post_media(bot, insta_conf['channels'], media)
 				time.sleep(2)
-		config.state.instagram[insta_conf['user_id']] = data[-1]['timestamp']
-		config.state.save()
+
+		new_last_timestamp = data[-1]['timestamp']
+		if new_last_timestamp > last_timestamp:
+			config.state.instagram[insta_conf['user_id']] = new_last_timestamp
+			config.state.save()
 
 def post_media(bot, channel_ids, media):
 	embed = {

commit 99d6a2f14096470c7ed7277b001af89078e8531a

instagram: fix saving last_timestamp of new accounts
broken in db98ba9

instagram.py
@@ -29,7 +29,7 @@ def new_media(bot):
 				time.sleep(2)
 
 		new_last_timestamp = data[-1]['timestamp']
-		if new_last_timestamp > last_timestamp:
+		if last_timestamp is None or new_last_timestamp > last_timestamp:
 			config.state.instagram[insta_conf['user_id']] = new_last_timestamp
 			config.state.save()
 