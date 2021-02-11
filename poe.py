lgbtqia-portugal/sbot

commit 21cb8c5ee7866c68aabdb52f3f2011a733d4d3c3

poe: show icons for currency

poe.py
@@ -105,15 +105,17 @@ def _search(league, q):
 		name = line.get('name')
 		if name is None:
 			name = line['currencyTypeName']
+			(detail,) = (d for d in data['currencyDetails'] if d['name'] == name)
+			line['icon'] = detail['icon']
 		if q in name.casefold():
 			names.add(name)
 			matches.append(line)
 			if q == name.casefold():
 				exact = True
+				break
 
 	if exact:
-		matches = [match for match in matches
-				if match.get('name', match.get('currencyTypeName')).casefold() == q]
+		matches = [matches[-1]]
 		names = {matches[0].get('name', matches[0].get('currencyTypeName'))}
 	return names, matches
 

commit 82dcfb23f9211b1f7a09a91edc180a295f1ee004

poe: re-fix 5L/6L exact matches
broken in 21cb8c5ee7866c68aabdb52f3f2011a733d4d3c3

poe.py
@@ -112,10 +112,12 @@ def _search(league, q):
 			matches.append(line)
 			if q == name.casefold():
 				exact = True
-				break
+				# there may be other exact matches (5L/6L), so we cannot break
 
 	if exact:
-		matches = [matches[-1]]
+		# there may be multiple lines that are exact matches (5L/6L)
+		matches = [match for match in matches
+				if match.get('name', match.get('currencyTypeName')).casefold() == q]
 		names = {matches[0].get('name', matches[0].get('currencyTypeName'))}
 	return names, matches
 